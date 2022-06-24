c6c888133631ff3e8dc0bdda6bc6337002353870
Wilton Leonam <leo.wiltons@gmail.com>
2022-06-24 11:24:52 -0300

    Primeiro Commit

---
+++ ex008.js
@@ -0,0 +1,8 @@
+var idade = 12
+if(idade < 16 ){
+  console.log('você não vota !')
+}else if(idade < 18 || idade >65 ){
+   console.log('Voto Opcional !')
+}else{
+   console.log('Você Obrigatório !')
+}
